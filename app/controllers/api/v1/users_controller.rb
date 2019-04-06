99c08ca71cd639df55b73046debe6e0024d29783
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-04-06 18:00:32 -0400

    added update for users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :get_user, only: [:update]
 
   def index
     render json: User.all
   end
 
-  def show
-    render json: User.find(params[:id]), status: :accepted
-  end
+  # def show
+  #   render json: User.find(params[:id]), status: :accepted
+  # end
 
   def profile
     token = request.headers["Authentication"].split(' ')[1]
@@ -30,10 +31,27 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def update
+    @user.update(user_params)
+    if @user.valid?
+      render json: {
+        user: UserSerializer.new(@user)
+      }, status: :updated
+    else
+      render json: {
+        error: 'failed to update user'
+      }, status: :not_acceptable
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :password, :bio, :avatar, :wins, :losses, :games_played, :highest_score)
+  end
+
+  def get_user
+    @user = User.find(params[:id])
   end
 
 end
