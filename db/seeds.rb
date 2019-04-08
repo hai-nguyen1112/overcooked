680d0e4a3dd7028b1eb77382522bdd3af23c58b9
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-04-07 22:28:35 -0400

    added seed data for level

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,8 @@ hai = User.create(kind: 'user', username: 'hai', password: "123456", bio: "I kno
 
 # Seed data for plates
 clean_plate = Plate.create(name: 'clean_plate', image: "https://i.ibb.co/wCfjbd6/clean-plate.png", kind: 'tool')
+dirty_plate = Plate.create(name: 'dirty_plate', image: "https://i.ibb.co/0hLDJ2p/dirty-plate.png", kind: 'dirty_tool')
+broken_plate = Plate.create(name: 'broken_plate', image: "https://i.ibb.co/kMMb0Vp/broken-plate.png", kind: 'broken_tool')
 # End of seed data for plates
 
 # Seed data for level 1
@@ -31,5 +33,24 @@ RecipeIngredient.create(recipe: cherries_recipe, ingredient: cherries)
 RecipeIngredient.create(recipe: kiwi_recipe, ingredient: kiwi)
 
 LevelPlate.create(level: level1, plate: clean_plate)
-
 # End of seed data for level 1
+
+# Seed data for level 2
+level2 = Level.create(kind: "level", name: "level2", clock: 90, qualified_points: 100, logo: "https://i.ibb.co/4TczWs1/logo-level2.png", trash_can: "https://i.ibb.co/M7mn9HD/wastebasket-1.png")
+
+pancakes_recipe = Recipe.create(level: level2, name: 'pancakes', image: "https://i.ibb.co/HDxksXB/pancakes-recipe.png", instruction: "https://i.ibb.co/Tcc4Ybv/instruction-pancakes.png", kind: 'recipe')
+doughnut_recipe = Recipe.create(level: level2, name: 'doughnut', image: "https://i.ibb.co/3kKskdR/doughnut-recipe.png", instruction: "https://i.ibb.co/PFPKq5z/instruction-doughnut.png", kind: 'recipe')
+shortcake_recipe = Recipe.create(level: level2, name: 'shortcake', image: "https://i.ibb.co/4JgsW6g/shortcake-recipe.png", instruction: "https://i.ibb.co/x3y75W0/instruction-shortcake.png", kind: 'recipe')
+
+pancakes = Ingredient.create(name: 'pancakes', image: "https://i.ibb.co/GvVhZF6/pancakes.png", kind:'ingredient')
+doughnut = Ingredient.create(name: 'doughnut', image: "https://i.ibb.co/BNZnZtg/doughnut.png", kind:'ingredient')
+shortcake = Ingredient.create(name: 'shortcake', image: "https://i.ibb.co/Cb8GkHC/shortcake.png", kind:'ingredient')
+
+RecipeIngredient.create(recipe: pancakes_recipe, ingredient: pancakes)
+RecipeIngredient.create(recipe: doughnut_recipe, ingredient: doughnut)
+RecipeIngredient.create(recipe: shortcake_recipe, ingredient: shortcake)
+
+LevelPlate.create(level: level2, plate: clean_plate)
+LevelPlate.create(level: level2, plate: dirty_plate)
+LevelPlate.create(level: level2, plate: broken_plate)
+# End of seed data for level 2
