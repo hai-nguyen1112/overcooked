b19e5d31c603b05013d8d1adad7fbec51288008e
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-04-15 23:15:02 -0400

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   validates :username, uniqueness: {case_sensitive: false}
   validates :username, presence: true, length: {maximum: 20}
   validates :password, presence: true, length: {in: 6..20}
-  validates :bio, presence: true, length: {in: 30..100}
+  validates :bio, presence: true, length: {maximum: 100}
   validates :avatar, presence: true
 end
